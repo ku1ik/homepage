baeb914d73f1d301b7e26b10e67f6e569d815258
Marcin Kulik <marcin.kulik@gmail.com>
2009-09-23 01:04:09 +0200

    initial commit

---
+++ javascripts/sickill.js
@@ -0,0 +1,7 @@
+$(function() {
+//  $('#content .article:odd,#content .article-preview:odd').addClass('odd');
+//  $('#content .article:even,#content .article-preview:even').addClass('even');
+  $(function() {
+    $('.images a').lightBox();
+  });
+});
