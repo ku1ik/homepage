63cf2de287e9913b734acdbc652bf968a799d4b7
Marcin Kulik <marcin.kulik@gmail.com>
2009-09-28 00:16:52 +0200

    completed tag pages

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -53,3 +53,17 @@ end
 def item(identifier)
   @items.find { |item| item.identifier == identifier }
 end
+
+def tags_and_items(items=nil)
+  @items = items unless items.nil?
+  articles.inject(Hash.new { |h,k| h[k] = [] }) do |acc, a|
+    a.tags.each do |t| 
+      acc[t] << a
+    end
+    acc
+  end
+end
+
+def articles_tagged_with(tag)
+  sorted_articles.select { |a| a.tags.include?(tag) }
+end
