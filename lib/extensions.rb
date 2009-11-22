e1225729b5bbbcdabe84f95f6fdfbd6d14bea821
Marcin Kulik <marcin.kulik@gmail.com>
2009-11-22 12:18:56 +0100

    added related posts box to post page

--- lib/extensions.rb
+++ lib/extensions.rb
@@ -1,3 +1,15 @@
+class Array
+  def blank?
+    empty?
+  end
+end
+
+class NilClass
+  def blank?
+    true
+  end
+end
+
 class Nanoc3::Item
   def tags
     self[:tags] && self[:tags].split(",").map { |t| t.strip }.sort || []
--- lib/posts.rb
+++ lib/posts.rb
@@ -9,18 +9,41 @@ end
 
 def render_post(post, opts={}, &blk)
   html = ""
+  html << %(<div class="section">) # open section
+
   html << %(<div class="article">)
+  # header
   html << render_post_header(post)
   html << %(  <br class="clear" />)
+  # content
   html << %(<div class="article-content">)
   html << (block_given? ? capture(&blk) : post.content)
-  html << %(</div>)
-  if opts[:comments]
-    html << %(<div id="disqus_thread"></div><script type="text/javascript" src="http://disqus.com/forums/sickill/embed.js"></script><noscript><a href="http://sickill.disqus.com/?url=ref">View the discussion thread.</a></noscript><a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>)
-  else
+  html << %(</div>) # close article-content
+  html << %(</div>) # close article
+  if !opts[:comments]
     html << link_to("View Comments", post.path + "#disqus_thread")
   end
-  html << %(</div>)
+  
+  # related posts
+  if opts[:comments] && !(related = related_articles(@item)).empty?
+    html << %(</div><div class="section">) # close and reopen section
+    html << %(<div class="related">)
+    html << %(<p>Somewhat related articles:</p>)
+    html << %(<ul>)
+    related.each do |a|
+      html << %(<li>#{link_to a[:title], a.path}</li>)
+    end
+    html << %(</ul>)
+    html << %(</div>) # close related
+  end
+  
+  # comments
+  if opts[:comments]
+    html << %(</div><div class="section">) # close and reopen section
+    html << %(<div id="disqus_thread"></div><script type="text/javascript" src="http://disqus.com/forums/sickill/embed.js"></script><noscript><a href="http://sickill.disqus.com/?url=ref">View the discussion thread.</a></noscript><a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>)
+  end
+  
+  html << %(</div>) # close section
   
   # Append to erbout if we have a block
   if block_given?
@@ -54,4 +77,20 @@ end
 
 def articles_for_tag(tag)
   sorted_articles.select { |a| a.tags.include?(tag) }
+end
+
+def related_articles(item, number=3)
+  matches = []
+  articles.reject { |a| a == item }.each do |a|
+    if !item.tags.blank? && !a.tags.blank?
+      common_tags = item.tags & a.tags
+      if common_tags.size >= 2
+        matches << [common_tags, a]
+        # p common_tags
+      end
+    end
+  end
+  best_matching = matches.sort_by { |m| -m.first.size }[0..2].map { |m| m[1] }
+  # p best_matching.map { |bm| bm[:title] }
+  # best_matching
 end
